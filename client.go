ff908898e8950a779605d726d496ca9797d2d5a4
bcmmbaga <b@infolabs.dev>
2020-02-25 15:19:56 +0300

    create new sms service without pass and username

--- client.go
+++ client.go
@@ -1,50 +1,36 @@
 package opes
 
-import (
-	"bytes"
-	"encoding/json"
-	"fmt"
-	"log"
-	"net/http"
-)
-
-var smsEndpoint = "https://sms.opestechnologies.co.tz/api/messages/send"
-
 // Service domain implementation for opes SMS API.
 type Service struct {
-	username string
-	password string
-	auth     *auth
+	auth *auth
 }
 
-// NewService ...
-func NewService(username, password string) *Service {
+// NewSMSService ...
+func NewSMSService() *Service {
 	return &Service{
-		username: username,
-		password: password,
-		auth:     generateToken(username, password),
+		auth: generateToken(),
 	}
 }
 
-// Send ...
-func (s *Service) Send(msgs ...Message) (smsResponse *SMSResponse) {
-	if ok := s.auth.isvalid(); !ok {
-		if err := s.refreshToken(); err != nil {
-			log.Fatalf(err.Error())
-		}
-	}
+// // Send ...
+// func (s *Service) Send(msgs ...Message) (smsResponse *SMSResponse) {
+// 	if ok := s.auth.isvalid(); !ok {
+// 		if err := s.refreshToken(); err != nil {
+// 			log.Fatalf(err.Error())
+// 		}
+// 	}
 
-	content, _ := json.Marshal(&msgs)
-	req, _ := http.NewRequest("POST", smsEndpoint, bytes.NewBuffer(content))
-	req.Header.Add("Authorization", fmt.Sprintf("Bearer %s", s.auth.token))
-	req.Header.Set("Content-Type", "application/json")
+// 	content, _ := json.Marshal(&msgs)
+// 	req, _ := http.NewRequest("POST", "", bytes.NewBuffer(content))
+// 	req.Header.Add("Authorization", fmt.Sprintf("Bearer %s", s.auth.token))
+// 	req.Header.Set("Content-Type", "application/json")
 
-	resp, _ := http.DefaultClient.Do(req)
-	defer resp.Body.Close()
+// 	resp, _ := http.DefaultClient.Do(req)
+// 	defer resp.Body.Close()
 
-	if err := json.NewDecoder(resp.Body).Decode(smsResponse); err != nil {
-		return
-	}
+// 	if err := json.NewDecoder(resp.Body).Decode(smsResponse); err != nil {
+// 		return
+// 	}
 
-	return
-}
+// 	return
+// }
