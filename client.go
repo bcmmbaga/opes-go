b0ea6e7e72d6b7ff22de43a5c86e66d0414c30dd
bcmmbaga <b@infolabs.dev>
2020-03-03 19:45:23 +0300

    send message

--- client.go
+++ client.go
@@ -65,29 +65,6 @@ func NewSMSService() *Service {
 	return svc
 }
 
-// // Send ...
-// func (s *Service) Send(msgs ...Message) (smsResponse *SMSResponse) {
-// 	if ok := s.auth.isvalid(); !ok {
-// 		if err := s.refreshToken(); err != nil {
-// 			log.Fatalf(err.Error())
-// 		}
-// 	}
-
-// 	content, _ := json.Marshal(&msgs)
-// 	req, _ := http.NewRequest("POST", "", bytes.NewBuffer(content))
-// 	req.Header.Add("Authorization", fmt.Sprintf("Bearer %s", s.auth.token))
-// 	req.Header.Set("Content-Type", "application/json")
-
-// 	resp, _ := http.DefaultClient.Do(req)
-// 	defer resp.Body.Close()
-
-// 	if err := json.NewDecoder(resp.Body).Decode(smsResponse); err != nil {
-// 		return
-// 	}
-
-// 	return
-// }
-
 // Send message
 func (s Service) Send(msgs ...Message) *SMSResponse {
 	url := "https://sms.opestechnologies.co.tz/api/messages/send"
